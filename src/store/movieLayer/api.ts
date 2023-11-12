995a36b1cbd93ecbd6536af13a2ab3ee7aed9c55
Hamersly <maksimwanderer96@gmail.com>
2023-11-13 01:18:25 +0500

    add favicon and logo

--- src/store/movieLayer/api.ts
+++ src/store/movieLayer/api.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 import {IDetailResponse, IListResponse, ISearchResponse} from "../../types/types";
 
+
+const proxy = {
+    protocol: 'https',
+    host: '142.93.151.99',
+    port: 45365,
+  }
+
 export async function listApi(
   format: string,
   sort: string,
@@ -11,7 +18,10 @@ export async function listApi(
   const url: string = `${BASE_URL}/${format}?api_key=${API_KEY}&language=ru-RU&sort_by=${sort}.desc&page=${listPage}`;
 
   const {page, results, total_pages}: IListResponse = await axios
-    .get(url)
+    .get(url, {
+      proxy: proxy,
+      responseType: 'json',
+    })
     .then((response: any) => response.data)
     .catch((error) => console.log(error));
 
@@ -24,7 +34,10 @@ export async function detailApi(format: string, id: string): Promise<object> {
   const url: string = `${BASE_URL}/${format}/${id}?api_key=${API_KEY}&language=ru-RU`;
 
   const {title, original_title, name, original_name, backdrop_path, overview}: IDetailResponse = await axios
-    .get(url)
+    .get(url, {
+      proxy: proxy,
+      responseType: 'json',
+    })
     .then((response: any) => response.data)
     .catch((error) => console.log(error));
 
@@ -37,7 +50,10 @@ export async function searchApi(query: string, queryPage: number): Promise<objec
   const url: string = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=ru-RU&query=${query}&page=${queryPage}`;
 
   const {page, results, total_pages}: ISearchResponse = await axios
-    .get(url)
+    .get(url, {
+      proxy: proxy,
+      responseType: 'json',
+    })
     .then((response: any) => response.data)
     .catch((error) => console.log(error));
 
@@ -51,10 +67,14 @@ export async function topApi(): Promise<object> {
   const url: string = `${BASE_URL}/discover/movie?api_key=${API_KEY}&language=ru-RU&sort_by=popularity.desc`;
 
   const response = await axios
-    .get(url)
+    .get(url, {
+      proxy: proxy,
+      responseType: 'json',
+    })
     .then((response: any) => response.data)
     .catch((error) => console.log(error));
 
   return response.results
 
 }
+
